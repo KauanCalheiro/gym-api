a7d12aeed854db599ba5fe02cfc082fba9cc2731
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2025-10-07 09:21:08 +0100

    feat: add muscle-group route

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Location\CityController;
 use App\Http\Controllers\Location\CountryController;
 use App\Http\Controllers\Location\StateController;
+use App\Http\Controllers\MuscleGroupController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,5 +30,7 @@ Route::prefix('v1')->group(function () {
         Route::apiResource('country', CountryController::class)->names('country');
         Route::apiResource('state', StateController::class)->names('state');
         Route::apiResource('city', CityController::class)->names('city');
+
+        Route::apiResource('muscle-group', MuscleGroupController::class)->names('muscle-group');
     });
 });
