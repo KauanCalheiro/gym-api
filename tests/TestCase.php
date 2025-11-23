57e0a9fc7c39107cdc2cd23d078cc09a1ab19112
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2025-11-23 10:02:11 +0000

    fix: remove seeding for all test cases

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,17 +2,8 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use RefreshDatabase;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->seed();
-    }
 }
